e5c32f73f53f09d5a0d917806d0c2ba53d328c33
luckyligovi <lucky.luvihi@student.moringaschool.com>
2024-08-28 20:10:58 +0300

    delete task

--- to-do.js
+++ to-do.js
@@ -9,8 +9,22 @@ function addTask() {
         let li = document.createElement("li");
         li.innerHTML = texBoxValue.value;
         listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
     }
 
-    texBoxValue.value = ""
-
 }
+
+texBoxValue.value = ""
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.classList.toggle("checked")
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove()
+
+    }
+},false)
+
